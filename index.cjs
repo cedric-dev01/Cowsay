dea2a192d7c901fed2a02ac7702a0da38d9ccf1c
cedric.dev01 <cedric.dev01@gmail.com>
2022-03-18 14:50:41 +0100

    Travail Cowsay

---
+++ index.cjs
@@ -0,0 +1,10 @@
+//import userInfo from './information.js';
+//console.log(userInfo);
+
+cowsay = require("cowsay");
+
+console.log(cowsay.say({
+    text : "I'm a moooodule",
+    e : "oO",
+    T : "U "
+}));
---
+++ information.js
@@ -0,0 +1,6 @@
+const userInfo = {
+  name:"Cédric",
+  campus:"Wild bz"
+}
+
+export default userInfo;
